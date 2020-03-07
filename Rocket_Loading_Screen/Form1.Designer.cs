a3fd54ba03056de9788c1a883c7ab486da2f1d8e
HyoBae <59951096+HyoilBae@users.noreply.github.com>
2020-03-06 17:07:15 -0800

    Add files via upload

---
+++ Rocket_Loading_Screen/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace Rocket_Loading_Screen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlLoadingBar = new System.Windows.Forms.Panel();
+            this.lbLoading = new System.Windows.Forms.Label();
+            this.picRocket = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picRocket)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlLoadingBar
+            // 
+            this.pnlLoadingBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(243)))), ((int)(((byte)(255)))));
+            this.pnlLoadingBar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pnlLoadingBar.Location = new System.Drawing.Point(50, 321);
+            this.pnlLoadingBar.Name = "pnlLoadingBar";
+            this.pnlLoadingBar.Size = new System.Drawing.Size(0, 5);
+            this.pnlLoadingBar.TabIndex = 2;
+            // 
+            // lbLoading
+            // 
+            this.lbLoading.AutoSize = true;
+            this.lbLoading.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.22642F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLoading.ForeColor = System.Drawing.Color.White;
+            this.lbLoading.Location = new System.Drawing.Point(346, 345);
+            this.lbLoading.Name = "lbLoading";
+            this.lbLoading.Size = new System.Drawing.Size(118, 24);
+            this.lbLoading.TabIndex = 0;
+            this.lbLoading.Text = "Loading 0%";
+            // 
+            // picRocket
+            // 
+            this.picRocket.BackgroundImage = global::Rocket_Loading_Screen.Properties.Resources.rocket_size_48;
+            this.picRocket.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picRocket.ErrorImage = null;
+            this.picRocket.Location = new System.Drawing.Point(350, 202);
+            this.picRocket.Name = "picRocket";
+            this.picRocket.Size = new System.Drawing.Size(100, 100);
+            this.picRocket.TabIndex = 0;
+            this.picRocket.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(40)))), ((int)(((byte)(46)))));
+            this.ClientSize = new System.Drawing.Size(800, 500);
+            this.Controls.Add(this.lbLoading);
+            this.Controls.Add(this.pnlLoadingBar);
+            this.Controls.Add(this.picRocket);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picRocket)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picRocket;
+        private System.Windows.Forms.Panel pnlLoadingBar;
+        private System.Windows.Forms.Label lbLoading;
+    }
+}
+
---
+++ Rocket_Loading_Screen/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rocket_Loading_Screen
+{
+    public partial class Form1 : Form
+    {
+        Timer LoadingTimer = new Timer();
+        Timer RocketTimer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void Default()
+        {
+            lbLoading.Text = "Loading 0%";
+            pnlLoadingBar.Width = 0;
+            picRocket.Top = 202;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadingTimer.Tick += LoadingTimer_Tick;
+            RocketTimer.Tick += RocketTimer_Tick;
+            LoadingTimer.Interval = 1;
+            RocketTimer.Interval = 1;
+            LoadingTimer.Start();
+        }
+
+        private void RocketTimer_Tick(object sender, EventArgs e)
+        {
+            if (picRocket.Top <= 5)
+            {
+                picRocket.Top = 202;
+                picRocket.BackgroundImage = Rocket_Loading_Screen.Properties.Resources.rocket_size_48;
+                RocketTimer.Stop();
+                Default();
+                LoadingTimer.Start();
+            }
+            else
+            {
+                picRocket.Top -= 5;
+            }
+        }
+
+        private void LoadingTimer_Tick(object sender, EventArgs e)
+        {
+            if (pnlLoadingBar.Width < 700)
+            {
+                pnlLoadingBar.Width += 7;
+                lbLoading.Text = "Loading " + (pnlLoadingBar.Width / 7).ToString() + "%";
+            }
+            else
+            {
+                picRocket.BackgroundImage = Rocket_Loading_Screen.Properties.Resources.rocket_size_48;
+                LoadingTimer.Stop();
+                RocketTimer.Start();
+            }
+        }
+    }
+}
